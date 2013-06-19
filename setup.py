e5957a299a8a5c1121b1ab004a1991af310c3e98
Matej Cotman <cotman.matej@gmail.com>
2013-06-19 19:32:00 +0200

    setup files

---
+++ setup.py
@@ -0,0 +1,75 @@
+# -*- coding: utf-8 -*-
+"""Installer for the plone.hud package."""
+
+from setuptools import find_packages
+from setuptools import setup
+
+import os
+
+
+def read(*rnames):
+    return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
+
+long_description = \
+    read('README.rst') + \
+    read('docs', 'CHANGELOG.rst') + \
+    read('docs', 'LICENSE.rst')
+
+setup(
+    name='plone.hud',
+    version='0.1',
+    description="Plone Heads Up Display Framework",
+    long_description=long_description,
+    # Get more from http://pypi.python.org/pypi?%3Aaction=list_classifiers
+    classifiers=[
+        "Framework :: Plone",
+        "Programming Language :: Python",
+        "Environment :: Web Environment",
+        "Operating System :: OS Independent",
+    ],
+    keywords='Plone HUD',
+    author='Matej Cotman',
+    author_email='cotman.matej@gmail.com',
+    url='http://pypi.python.org/pypi/plone.hud',
+    license='BSD',
+    packages=find_packages('src', exclude=['ez_setup']),
+    namespace_packages=['plone'],
+    package_dir={'': 'src'},
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[
+        'five.grok',
+        'five.pt',
+        'Pillow',
+        'Plone',
+        'plone.api',
+        'setuptools',
+        'z3c.jbot',
+    ],
+    extras_require={
+        'test': [
+            'mock',
+            'plone.app.testing',
+            'unittest2',
+        ],
+        'develop': [
+            'coverage',
+            'flake8',
+            'jarn.mkrelease',
+            'niteoweb.loginas',
+            'plone.app.debugtoolbar',
+            'plone.reload',
+            'Products.Clouseau',
+            'Products.DocFinderTab',
+            'Products.PDBDebugMode',
+            'Products.PrintingMailHost',
+            'Sphinx',
+            'zest.releaser',
+            'zptlint',
+        ],
+    },
+    entry_points="""
+    [z3c.autoinclude.plugin]
+    target = plone
+    """,
+)
