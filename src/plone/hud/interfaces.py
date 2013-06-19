87c64e4d954815174998d03182b0c69151964ec7
Matej Cotman <cotman.matej@gmail.com>
2013-06-20 01:53:21 +0200

    add IHUDSettings interface

--- src/plone/hud/interfaces.py
+++ src/plone/hud/interfaces.py
@@ -2,7 +2,18 @@
 """Module where all interfaces, events and exceptions live."""
 
 from plone.theme.interfaces import IDefaultPloneLayer
+from zope import schema
+from zope.interface import Interface
 
 
 class IPloneHudLayer(IDefaultPloneLayer):
     """Marker interface that defines a Zope 3 browser layer."""
+
+
+class IHUDSettings(Interface):
+    """ Define settings data structure """
+
+    adminLanguage = schema.TextLine(
+        title=u"Admin language",
+        default=u"EN"
+    )
